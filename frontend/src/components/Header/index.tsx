ab58918707d21d790a9e24769971e9a81eb21eed
eduardo-assimo <eduardo.assimo@gmail.com>
2022-09-10 23:01:15 -0300

    Commit Final

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -10,7 +10,7 @@ function Header() {
                 <h1>DSMeta</h1>
                 <p>
                     Desenvolvido por
-                    <a href="https://www.linkedin.com/in/eduardo-assimo/">Eduardo Assimo</a>
+                    <a href="https://www.linkedin.com/in/eduardo-assimo/"> Eduardo Assimo</a>
                 </p>
             </div>
         </header>
